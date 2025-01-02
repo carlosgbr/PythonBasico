54ae25270340ae3b9a6feb93f023363a832f377f
carlosgbr <carlosgbr@gmail.com>
2025-01-02 15:47:25 -0600

    Create muestra_letras.py
    Programa en Python en donde Se ilustra el bucle for para recorrer cadenas de texto

---
+++ muestra_letras.py
@@ -0,0 +1,17 @@
+""" Loops: muestra_letras.py
+ Se ilustra el bucle for para recorrer cadenas de texto
+ Por https://about.me/carlosgbr
+ Fecha: 02/01/2025
+ Versión 1
+ Actualizado: 02/01/2025
+ Para Python 3.11 y superior
+"""
+"""
+ENUNCIADO: La función muestra_letras debe imprimir cada letra de una palabra
+en una línea separada. Complete los espacios en blanco para que eso suceda.
+"""
+def muestra_letras(palabra):
+	for letra in palabra:
+		print(letra)
+muestra_letras("Hola")
+# Debe imprimir una letra por línea

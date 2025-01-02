a962d005ed1a60772af5fa5d8c28ddbe9c0d03e5
carlosgbr <carlosgbr@gmail.com>
2025-01-02 15:48:43 -0600

    Create Expresiones_Variables01.py
    Python - Ejemplo - POC - Ejemplo del uso de Expresiones y Variables en Python, puedes descargar todos los ejemplos en nuestro repositorio en Github

---
+++ Expresiones_Variables01.py
@@ -0,0 +1,13 @@
+""" Expresiones y Variables: Expresiones_Variables01.py
+ Se ilustra el uso de variables y expresiones en Python
+ Por https://about.me/carlosgbr
+ Fecha: 02/01/2025
+ Versión 1
+ Actualizado: 02/01/2025
+ Para Python 3.11 y superior
+"""
+cuenta = 47.28
+subt = cuenta * 0.15
+total = cuenta + subt
+parte = total/2
+print("Cada persona debe pagar: " + str(parte))

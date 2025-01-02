31a9fb37d3cbf3d6acf32d44b398aa658208a87f
carlosgbr <carlosgbr@gmail.com>
2025-01-02 15:02:34 -0600

    Create numeros_en_linea.py
    ENUNCIADO: Complete los espacios en blanco de este código para imprimir los 
    números del 1 al 7. Los números deben estar en la misma línea

---
+++ numeros_en_linea.py
@@ -0,0 +1,17 @@
+""" Loops: numeros_en_linea.py
+Se muestran los números 1 a 7 en la misma línea, se ilustra el uso de end 
+dentro de print()
+Por https://about.me/carlosgbr
+Fecha: 02/01/2025
+Versión 1
+Actualizado: 02/01/2025
+Para Python 3.11 y superior
+"""
+"""
+ENUNCIADO: Complete los espacios en blanco de este código para imprimir los 
+números del 1 al 7. Los números deben estar en la misma línea
+"""
+number = 1
+while number <= 7:
+	print(number, end=" ")
+	number += 1
